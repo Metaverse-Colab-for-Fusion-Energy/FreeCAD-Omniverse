eace43f44a649d42f12f1445ddfb9e80dc285b47
raska-s <raska_soemantoro@outlook.com>
2024-04-30 14:46:29 +0100

    robocopy stuff fixed, but delete still doesnt work properly

--- install.bat
+++ install.bat
@@ -1,9 +1,14 @@
 @echo off
  
 echo 'Beginning installation of FreeCAD-Omniverse Connector'
+
+echo 'Pulling Connect Sample v202.0'
  
 git clone https://github.com/raska-s/ovConnectSample.git temp
 
-robocopy .\temp .\omniConnect /XO
 
-del .\temp
+Remove-Item -LiteralPath ".\temp\.git" -Force -Recurse
+
+robocopy .\temp .\omniConnect /e
+
+Remove-Item -LiteralPath ".\temp" -Force -Recurse
