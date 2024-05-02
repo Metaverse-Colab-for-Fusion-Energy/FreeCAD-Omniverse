694803017ec73a6cdde1fa7a46d82956074bebd3
raska-s <raska_soemantoro@outlook.com>
2024-05-02 13:28:56 +0100

    robocopy make quiet

--- install.bat
+++ install.bat
@@ -1,23 +1,23 @@
 @echo off
  
-echo Beginning installation of FreeCAD-Omniverse Connector
+echo ===Beginning installation of FreeCAD-Omniverse Connector===
 
-echo Pulling Connect Sample v202.0...
+echo --- Pulling Connect Sample v202.0...
  
 git clone https://github.com/raska-s/ovConnectSample.git temp
 
 rd ".\temp\.git\" /q /s
 
-robocopy .\temp .\omniConnect /e
+robocopy .\temp .\omniConnect /e /NFL /NDL /NJH /NJS 
 
-echo Cleaning dependency directory...
+echo --- Cleaning dependency directory...
 
 rd ".\temp\" /s /q
 
-echo Building Connect Sample...
+echo --- Fetching Connect Sample dependencies...
 
 call .\omniConnect\repo.bat build --fetch-only
 
-echo Installing Python dependencies...
+echo --- Installing Python dependencies...
 
 .\omniConnect\_build\target-deps\python\python.exe -m pip install open3d aioconsole
