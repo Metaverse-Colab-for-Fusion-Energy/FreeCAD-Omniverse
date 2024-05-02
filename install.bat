fd6ba9553749c4e0f688e0b37aa63f43af6f71d9
raska-s <raska_soemantoro@outlook.com>
2024-05-02 11:35:58 +0100

    auto install script now pulls and installs deps for python and omniconnect

--- install.bat
+++ install.bat
@@ -1,14 +1,23 @@
 @echo off
  
-echo 'Beginning installation of FreeCAD-Omniverse Connector'
+echo Beginning installation of FreeCAD-Omniverse Connector
 
-echo 'Pulling Connect Sample v202.0'
+echo Pulling Connect Sample v202.0...
  
 git clone https://github.com/raska-s/ovConnectSample.git temp
-
 
 rd ".\temp\.git\" /q /s
 
 robocopy .\temp .\omniConnect /e
 
+echo Cleaning dependency directory...
+
 rd ".\temp\" /s /q
+
+echo Building Connect Sample...
+
+call .\omniConnect\repo.bat build --fetch-only
+
+echo Installing Python dependencies...
+
+.\omniConnect\_build\target-deps\python\python.exe -m pip install open3d aioconsole
